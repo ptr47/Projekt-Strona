ef8da6a23a154b903a3618b1a186101ec36aacd6
zdz437 <70442277+zdz437@users.noreply.github.com>
2023-11-04 19:32:23 +0100

    format

--- wpn_bd.php
+++ wpn_bd.php
@@ -1,42 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="resource/main.css" />
-    <link rel="stylesheet" href="resource/mobile.css" />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/icon?family=Material+Icons"
-    />
-    <title>Broń</title>
-  </head>
 
-  <body>
-    <div class="container">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="resource/main.css" />
+  <link rel="stylesheet" href="resource/mobile.css" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+  <title>Broń</title>
+</head>
+
+<body>
+  <div class="container">
     <header>
-        <div class="menu">
-          <a href="index.html">Home</a>
-          <button class="hamburgerMenu invisible">Menu</button>
-        </div>
-        <ul class="desktop">
-          <li><a href="#">Historia</a></li>
-          <li><a href="wpn_bd.php">Baza broni</a></li>
-          <li><a href="#">Sklep</a></li>
-          <li class="nav-right"><a href="#">Login</a></li>
-          <li class="nav-right"><a href="#">Koszyk</a></li>
-        </ul>
-      </header>
-      <main>
-        <div class="search">
-          <input /><button><img style="height: 100%; width: 100%;"src="img/magnifying-glass.png" alt="search"></button>
-        </div>
-        <br />content
-      </main>
-      <footer>
+      <div class="menu">
+        <a href="index.html">Home</a>
+        <button class="hamburgerMenu invisible">Menu</button>
+      </div>
+      <ul class="desktop">
+        <li><a href="#">Historia</a></li>
+        <li><a href="wpn_bd.php">Baza broni</a></li>
+        <li><a href="#">Sklep</a></li>
+        <li class="nav-right"><a href="#">Login</a></li>
+        <li class="nav-right"><a href="#">Koszyk</a></li>
+      </ul>
+    </header>
+    <main>
+      <div class="search">
+        <input /><button><img style="height: 100%; width: 100%;" src="img/magnifying-glass.png" alt="search"></button>
+      </div>
+      <br />content
+    </main>
+    <footer>
 
-      </footer>
-    </div>
-    <script src="scripts/responsive.js"></script>
-  </body>
+    </footer>
+  </div>
+  <script src="scripts/responsive.js"></script>
+</body>
+
 </html>
