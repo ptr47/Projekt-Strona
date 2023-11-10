be50465d346433a53e58208d7950966e2d7eea5a
zdz437 <70442277+zdz437@users.noreply.github.com>
2023-11-11 00:41:45 +0100

    navbar change

--- scripts/dropdownMenu.js
+++ scripts/dropdownMenu.js
@@ -1,15 +1,16 @@
+function showDropdown() {
+    document.getElementById("mobileDropdown").classList.toggle("m-show");
+}
+
 window.onclick = function (event) {
-    if (!event.target.matches("#hamburgerMenu")) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        for (var i = 0; i < dropdowns.length; i++) {
+    if (!event.target.matches('.m-dropbtn')) {
+        var dropdowns = document.getElementsByClassName("m-dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
             var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
+            if (openDropdown.classList.contains('m-show')) {
+                openDropdown.classList.remove('m-show');
             }
         }
     }
-}
-function dropdown()
-{
-    document.getElementById("myDropdown").classList.toggle("invisible");
 }
--- scripts/responsive.js
+++ scripts/responsive.js
@@ -4,10 +4,10 @@ function responsive()
     console.log(width)
     if(width<768)
     {
-        document.getElementById("hamburgerMenu").classList.remove("invisible")
+        document.getElementById("hamburgerMenu").classList.add("m-show")
     }
     else
     {
-        document.getElementById("hamburgerMenu").classList.add("invisible")
+        document.getElementById("hamburgerMenu").classList.remove("m-show")
     }
 }
