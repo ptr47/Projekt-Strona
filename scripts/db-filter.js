0be256dfeac6cd9289b1fecb9467954d224f8d7f
zdz437 <70442277+zdz437@users.noreply.github.com>
2023-11-23 02:24:17 +0100

    db filter works for real this time

--- scripts/db-filter.js
+++ scripts/db-filter.js
@@ -1,31 +1,21 @@
 function filter() {
-    var filter = document.getElementById('db-input').value.toUpperCase();
-    var type = 0; // 0 for weapons container, 1 for ammo container
-    if (document.getElementById("radio-ammo").checked) { type = 1; }
-    var tiles = document.getElementsByClassName('tile-container')[type].getElementsByClassName('tile');
-
-    for (var i = 0; i < tiles.length; i++) {
-        let p = tiles[i].getElementsByClassName("tile-name")[0];
-        if (p.textContent.toUpperCase().indexOf(filter) > -1) {
-            tiles[i].style.display = "";
-        } else {
-            tiles[i].style.display = "none";
-        }
-    }
-}
-
-function filterType() {
-    var type = 0, typeName = "db-type-wpn";
+    var type = 0, typeName = "db-type-wpn"; // 0 for weapons container, 1 for ammo container
+    
     if (document.getElementById("radio-ammo").checked) {
         type = 1;
         typeName = "db-type-ammo";
     }
-    var filter = document.getElementById(typeName).value;
+
+    var filterText = document.getElementById('db-input').value.toUpperCase();
+    var filterType = document.getElementById(typeName).value;
+
     var tiles = document.getElementsByClassName('tile-container')[type].getElementsByClassName('tile');
 
     for (var i = 0; i < tiles.length; i++) {
-        let p = tiles[i].getElementsByClassName("tile-type")[0];
-        if (p.textContent == filter || filter == "") {
+        let tileName = tiles[i].getElementsByClassName("tile-name")[0];
+        let tileType = tiles[i].getElementsByClassName("tile-type")[0];
+
+        if (tileName.textContent.toUpperCase().indexOf(filterText) > -1 && (tileType.textContent == filterType || filterType == "")) {
             tiles[i].style.display = "";
         } else {
             tiles[i].style.display = "none";
