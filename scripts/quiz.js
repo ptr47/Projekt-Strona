155b1f8b800968f369dace8c9ad878efdbdfa0e7
zdz437 <70442277+zdz437@users.noreply.github.com>
2023-11-26 17:19:13 +0100

    quiz

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,4 +1,15 @@
+$(function () {
+
+})
+
 function startQuiz() {
     $("#quiz-start").toggle();
     $(".quiz-container").toggle();
+    $("body").one("mouseleave", function () {
+        alert("Bez oszukiwania :)")
+    })
+}
+
+function endQuiz() {
+    $("body").off()
 }
