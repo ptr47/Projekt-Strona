d014645a77276cc44833d0aa2b18b128944c0c2d
zdz437 <70442277+zdz437@users.noreply.github.com>
2023-12-01 11:19:48 +0100

    progress

--- scripts/typeChange.js
+++ scripts/typeChange.js
@@ -1,6 +1,13 @@
-function checkValue()
-{
-    var select = document.getElementById("classification-select");
-    console.log(select.value);
+function checkValue() {
+    var select = document.getElementById("classification-select").value;
+    var classification = document.getElementsByClassName("classification")[0];
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementsByClassName("classification")[0].innerHTML = this.responseText;
+        }
+    };
+
+    xhttp.open("GET", "ajax_info.txt", true);
+    xhttp.send();
 }
-// change document with jQuery
