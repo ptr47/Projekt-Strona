0b4054d16b786a2203866b66b7588634555c4a0e
zdz437 <70442277+zdz437@users.noreply.github.com>
2023-12-02 13:43:22 +0100

    type change

--- scripts/typeChange.js
+++ scripts/typeChange.js
@@ -1,13 +1,42 @@
+$(function () {
+    checkValue();
+});
+
 function checkValue() {
-    var select = document.getElementById("classification-select").value;
-    var classification = document.getElementsByClassName("classification")[0];
+    var wybor = document.getElementById("classification-select");
+    var option = 0;
+    switch (wybor.value) {
+        case "Rodzaj broni":
+            option = 0;
+            break;
+        case "Automatyka broni":
+            option = 1;
+            break;
+        case "Tryb ognia broni":
+            option = 2;
+            break;
+        case "Rodzaj amunicji":
+            option = 3;
+            break;
+    }
     var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "resource/classification_content.xml", true);
+    xhttp.send();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementsByClassName("classification")[0].innerHTML = this.responseText;
+            loadData(this, option, wybor);
         }
     };
+}
 
-    xhttp.open("GET", "ajax_info.txt", true);
-    xhttp.send();
+function loadData(xml, option, title) {
+    var i;
+    var xmlDoc = xml.responseXML;
+    var content = "<h2>" + title.value + ":</h2> <ul>";
+    var item = xmlDoc.getElementsByTagName("TYPE")[option].getElementsByTagName("ITEM")
+    for (i = 0; i < item.length; i++) {
+        content += "<li><h4>" + item[i].getElementsByTagName("NAME")[0].textContent + "</h4>" + item[i].getElementsByTagName("DESC")[0].textContent + "</li>";
+    }
+    content += "</ul>";
+    document.getElementsByClassName("classification")[0].innerHTML = content;
 }
