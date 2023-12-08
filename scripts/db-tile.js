d0a892bc80c05f1cfc5de2cfafb65277be70151b
zdz437 <70442277+zdz437@users.noreply.github.com>
2023-12-08 13:49:22 +0100

    finish

--- scripts/db-tile.js
+++ scripts/db-tile.js
@@ -1,3 +1,17 @@
-function tileShow() {
+$(function () {
+   $(".modal").click(function () { 
+    $("#db-tile").hide();
+   });
+});
+function tileShow(event) {
+    var tile = event.target.parentElement
+    if (tile.id === "container-weapons") { tile = event.target}
+    console.log(tile);
+    var modal = document.getElementById("db-tile");
+    modal.style.display = "block";
+    var obraz = tile.firstElementChild.outerHTML
+    var nazwa = tile.getElementsByClassName("tile-name")[0].innerHTML
+    var typ = tile.getElementsByClassName("tile-type")[0].innerHTML
+    document.getElementById("tile-content").innerHTML = obraz +'    <p>Nazwa: ' + nazwa + '</p>    <p>Typ: ' +typ + '</p>'
 
 }
