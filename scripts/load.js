f5de3611af6b3ea0dbbede6fe7c7401470ef71db
zdz437 <70442277+zdz437@users.noreply.github.com>
2023-12-09 16:33:59 +0100

    load footer + delete old files

---
+++ scripts/load.js
@@ -0,0 +1,5 @@
+$(function () {
+    $("header").load("resource/navbar.html");
+    $("#include-login").load("user/login_modal.html");
+    $("footer").load("resource/footer.html");
+});
--- scripts/navbar.js
+++
@@ -1,4 +0,0 @@
-$(function () {
-    $("#include-navbar").load("resource/navbar.html");
-    $("#include-login").load("user/login_modal.html");
-});
