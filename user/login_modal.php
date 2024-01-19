6f7b248910814d8b4bbefdf988b15ee8d41c9317
zdz437 <70442277+zdz437@users.noreply.github.com>
2024-01-19 21:36:57 +0100

    base admin panel

--- user/login_modal.php
+++ user/login_modal.php
@@ -1,10 +1,20 @@
 <html>
-    <?php 
+<style>
+ .user-panel{
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+ }
+</style>
+<div class="user-panel">
+    <?php
     include("../session.php");
     echo 'Login: ';
     echo $_SESSION["login"];
+    if($_SESSION["login"]=="admin")
+    echo '<a href="user/admin.php">Panel administracyjny</a>'
     ?>
-    <br>
-    <br>
     <a href="user/logout.php">Wyloguj</a>
+</div>
+
 </html>
